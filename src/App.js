b4a962d35e4148373d19fba2cc25de4a83ca4b4f
Miqotes <ashleelennab@gmail.com>
2021-09-21 17:42:54 -0700

    added header

--- src/App.js
+++ src/App.js
@@ -22,7 +22,9 @@ const App = () => {
   }
 
   return (
+    
     <form class="ui center aligned container">
+      <h2 class="ui header"><img src="https://cdn.theatlantic.com/thumbor/mORGMuM_Oh1Rc3YJzLc3bwoApIY=/1071x0:3826x2755/1080x1080/media/img/mt/2021/07/GettyImages_473437806_copy/original.jpg" alt="" class1="ui circular image"/> Upload your image!</h2>
       <div class="ui hidden divider">
         {file.length > 0 &&
           file.map((item, index) => {
@@ -38,7 +40,7 @@ const App = () => {
       </div>
 
       <div class="ui hidden divider">
-        <input class="ui primary button"
+        <input 
           type="file"
           disabled={file.length === 5}
           className="form-control"
@@ -48,7 +50,8 @@ const App = () => {
       <br></br>
       <br></br>
       <br></br>
-      <button class="ui primary button"
+      <button 
+        class="ui primary button"
         type="button"
         className="btn btn-primary btn-block"
         onClick={upload}
